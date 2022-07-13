2323975709112ebf93bc4e37b32a808eec1d7d3b
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-07-13 18:13:26 -0300

    Iniciando criação da Interface Gráfica

--- src/main/java/com/capgemini/todoapp/main/Main.java
+++ src/main/java/com/capgemini/todoapp/main/Main.java
@@ -1,5 +1,6 @@
 package com.capgemini.todoapp.main;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.capgemini.todoapp.controller.ProjectController;
@@ -7,20 +8,23 @@ import com.capgemini.todoapp.model.Project;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		ProjectController projectController = new ProjectController();
 		
 		Project project = new Project();
-		project.setName("Projeto Teste");
-		project.setDescription("Este é um projeto criado para fins de teste");
-		projectController.save(project);
+		//project.setName("Projeto Teste 2");
+		//project.setDescription("Este é um projeto criado para fins de teste");
+		//projectController.save(project);
 		
-		project.setName("Novo nome do projeto teste");
-		projectController.update(project);
+		// project.setId(17);
+		// project.setName("Novo nome do projeto teste");
+		// projectController.update(project);
 		
 		List<Project> projects = projectController.getAll();
 		System.out.println("Total de projetos = "+ projects.size());
+		
+		// projectController.removeById(16);
 	}
 	
 
